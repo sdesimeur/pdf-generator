79b7e256a354e6afc2b6c37a1ce5fffe34c8a72f
Nolan Amy <nolan@plickers.com>
2013-07-25 23:38:57 -0700

    GeneratePDF - 2013-06-12

--- main.rb
+++ main.rb
@@ -29,7 +29,7 @@ class BarcodePDF
 			:assembly_scale => 2,
 			:assembly_position => {:x => 100, :y => 100}
 		}
-		@options = options.merge(default_options)
+		@options = default_options.merge(options)
 		@scale = @options[:assembly_scale]
 		@width = @height = @options[:barcode_size] * BARCODE_SIZE
 		@margin = [@options[:assembly_position][:x], 
@@ -46,76 +46,78 @@ class BarcodePDF
     	:normal => "arial.ttf"})
 	end
 
-	#Parameters:
-	# (Hash) *pages*: an hash that contains fill code for each page
-	# => key: page_number
-	# => value: fill code
 	def draw_barcode_assembly(
-		pages #barcode fill-array of all pages
+		fills, options = {} #barcode fill-array
 	)
-		#draw each page based on the fill codes
-		pages.keys.each_with_index do |page_number, index|
-			fills = pages[page_number]
-			@pdf.scale @scale
-			@options[:numbers] = Array.new(4, page_number)
-			@pdf.fill_color @options[:barcode_color]
-			@pdf.fill_rectangle([0, @height], @width, @height)
-
-			#Draw the empty box
-			@pdf.fill_color = "ffffff"
-			fills.each_with_index do |value, index|
-				if value == 1 #Need to be empty, draw white box
-					x = @options[:barcode_size]*FILL_CODE[index][0]
-					y = @height - (@options[:barcode_size]*FILL_CODE[index][1])
-					@pdf.fill_rectangle([x, y], @options[:barcode_size], @options[:barcode_size])
-				end
+		barcode_size = @options[:barcode_size]
+		options = @options.merge(options)
+		@pdf.scale @scale
+		
+		#Draw the boxes
+		fills.each_with_index do |value, index|
+			x = barcode_size*FILL_CODE[index][0]
+			y = @height - (barcode_size*FILL_CODE[index][1])
+			if value == 1 #Need to be empty, draw white box
+				@pdf.fill_color = "ffffff"
+			else
+				@pdf.fill_color options[:barcode_color]
 			end
+			@pdf.fill_rectangle([x, y], barcode_size, barcode_size)
+		end
 
-			#Draw 4 sides
-			angle = 0
-			translate_x = [0, @width, @width, 0]
-			translate_y = [@height, @height, 0, 0]
-			4.times do |i|
-				@pdf.save_graphics_state
-				@pdf.translate(translate_x[i], translate_y[i])
-				@pdf.rotate(angle, :origin => [0, 0]) do 
+		#Draw 4 sides
+		angle = 0
+		translate_x = [0, @width, @width, 0]
+		translate_y = [@height, @height, 0, 0]
+		4.times do |i|
+			@pdf.save_graphics_state
+			@pdf.translate(translate_x[i], translate_y[i])
+			@pdf.rotate(angle, :origin => [0, 0]) do 
 
-					#draw the annotation text
-					@pdf.font_size @options[:annotation_font][:size]
-					@pdf.font @options[:annotation_font][:face]
-					@pdf.fill_color @options[:annotation_font][:color]
-					@pdf.draw_text @options[:annotations][i], :at => [@options[:annotation_position][:x], @options[:annotation_position][:y]]
+				#draw the annotation text
+				@pdf.font_size options[:annotation_font][:size]
+				@pdf.font options[:annotation_font][:face]
+				@pdf.fill_color options[:annotation_font][:color]
+				@pdf.draw_text options[:annotations][i], 
+				:at => [options[:annotation_position][:x], options[:annotation_position][:y]]
 
-					#draw the answer text
-					@pdf.font_size @options[:answer_font][:size]
-					@pdf.font @options[:answer_font][:face]
-					@pdf.fill_color @options[:answer_font][:color]
-					answer = @options[:answers][i]
-					@pdf.draw_text answer, 
-						:at => [@width/2 - @pdf.width_of(answer) - @options[:answer_position][:x], 
-								@options[:answer_position][:y]] 
+				#draw the answer text
+				@pdf.font_size options[:answer_font][:size]
+				@pdf.font options[:answer_font][:face]
+				@pdf.fill_color options[:answer_font][:color]
+				answer = options[:answers][i]
+				@pdf.draw_text answer, 
+					:at => [@width/2 - @pdf.width_of(answer) - options[:answer_position][:x], 
+							options[:answer_position][:y]] 
 
-					#draw the number
-					@pdf.font_size @options[:number_font][:size]
-					@pdf.font @options[:number_font][:face]
-					@pdf.fill_color @options[:number_font][:color]
-					number = @options[:numbers][i]
-					@pdf.draw_text number, :at => [
-						@width - @pdf.width_of(number) + @options[:number_position][:x], 
-						@options[:number_position][:y]]
-				end
-				angle -= 90
-				@pdf.restore_graphics_state
+				#draw the number
+				@pdf.font_size options[:number_font][:size]
+				@pdf.font options[:number_font][:face]
+				@pdf.fill_color options[:number_font][:color]
+				number = options[:numbers][i]
+				@pdf.draw_text number, :at => [
+					@width - @pdf.width_of(number) + options[:number_position][:x], 
+					options[:number_position][:y]]
 			end
-
-			#Check if we need to start a new page or not
-			@pdf.start_new_page if index < pages.count - 1
+			angle -= 90
+			@pdf.restore_graphics_state
 		end
 	end
 
 	#Generate the PDF and save to disk
 	def save(file_name)
 		@pdf.render_file file_name
+	end
+	
+	def draw_card_set(pages)
+		count = 0
+		pages.each do |key, value|
+			draw_barcode_assembly(value, {:numbers => [key, key, key, key]})
+			if count < pages.keys.length - 1
+				@pdf.start_new_page
+			end
+			count += 1
+		end
 	end
 end
 
@@ -125,7 +127,7 @@ pages = {"0" => [0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 	"1" => [0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 	"2" => [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,1,0,0,0,0,0]}
 
-barcode.draw_barcode_assembly(pages)
+barcode.draw_card_set(pages)
 
 #Save to file
 barcode.save "test.pdf"
